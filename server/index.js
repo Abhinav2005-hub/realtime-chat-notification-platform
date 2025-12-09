28340647f2b49ca7a6395d58f85eb041e56de35b
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-12-09 23:36:57 +0530

    connect backend to postgres

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pkg from"pg";
 
 dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+async function testDB() {
+    try {
+        const res = await pool.query("SELECT NOW()");
+        console.log("Database connected at:", res.rows[0].now);
+    } catch (error) {
+        console.error("Database connection failed:", error.message);
+    }
+}
+
+testDB();
 
 const app = express();
 app.use(cors());
