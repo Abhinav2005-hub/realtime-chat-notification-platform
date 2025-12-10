ce4b48f1493fd2d9ea9f6af5bc09e4aa9457c161
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-12-10 21:43:03 +0530

    create auth routes

--- server/index.js
+++ server/index.js
@@ -1,30 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import pkg from"pg";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
-const { Pool } = pkg;
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
-
-async function testDB() {
-    try {
-        const res = await pool.query("SELECT NOW()");
-        console.log("Database connected at:", res.rows[0].now);
-    } catch (error) {
-        console.error("Database connection failed:", error.message);
-    }
-}
-
-testDB();
-
 const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req,res) => {
     res.send("Backend is running");
