0a54e0baa7967681ddbdabfa39e3139f7337a6f1
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-12-24 21:25:23 +0530

    stablise redis connection

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,3 +1,18 @@
 import Redis from "ioredis";
 
-export const redis = new Redis(process.env.REDIS_URL);
+export const redis = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+
+  maxRetriesPerRequest: null, // prevents crash
+  retryStrategy(times) {
+    if (times > 5) {
+      return null; // stop retrying after 5 attempts
+    }
+    return Math.min(times * 200, 1000);
+  }
+});
+
+redis.on("error", (err) => {
+  console.error("Redis error:", err.message);
+});
