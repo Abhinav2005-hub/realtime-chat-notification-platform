8a3bc07cae14b918677bb32f72cc122ac02f2390
Abhinav2005-hub <abhinavsaini995@gmail.com>
2026-01-17 23:55:50 +0530

    ensure JWT validation and userId injection

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -10,6 +10,7 @@ export const protect = (req, res, next) => {
   try {
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("JWT DECODED:", decoded);
     req.userId = decoded.userId;
     next();
   } catch (err) {
