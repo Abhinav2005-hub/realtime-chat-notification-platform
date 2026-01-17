b0324c6c39fb1572ca5b013372b838df3158b147
Abhinav2005-hub <abhinavsaini995@gmail.com>
2026-01-17 23:53:00 +0530

    clean up conversation routes and middleware usage

--- server/routes/conversationRoutes.js
+++ server/routes/conversationRoutes.js
@@ -2,30 +2,35 @@ import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { validateRequest } from "../middleware/validateRequest.js";
 import { createGroupSchema } from "../validators/conversationValidators.js";
-import { getConversations, createGroupConversation, createOneToOneConversation  } from "../controllers/conversationController.js";
+
+import {
+  getConversations,
+  createGroupConversation,
+  createOneToOneConversation,
+} from "../controllers/conversationController.js";
 
 const router = express.Router();
 
-// get all conversations of logged-in user
+/* GET ALL USER CONVERSATIONS */
 router.get(
-    "/",
-    protect,
-    getConversations,
+  "/",
+  protect,
+  getConversations
 );
 
-// group conversation
+/* CREATE 1-TO-1 CONVERSATION */
 router.post(
-    "/group",
-    protect,
-    validateRequest(createGroupSchema),
-    createGroupConversation
+  "/one-to-one",
+  protect,
+  createOneToOneConversation
 );
 
-//one-to-one conversation
+/* CREATE GROUP CONVERSATION */
 router.post(
-    "/one-to-one",
-    protect,
-    createOneToOneConversation
+  "/group",
+  protect,
+  validateRequest(createGroupSchema),
+  createGroupConversation
 );
 
 export default router;
