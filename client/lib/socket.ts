d172acb4df5fab584c5ad122a6e353fca15ba5c7
Abhinav2005-hub <abhinavsaini995@gmail.com>
2026-02-11 20:26:30 +0530

    send auth token in socket handshake

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,10 +1,20 @@
 import { io, Socket } from "socket.io-client";
 
-let socket: Socket | null = null;
+let socket: any;
 
 export const connectSocket = (token: string) => {
   socket = io("http://localhost:5000", {
-    auth: { token },
+    auth: {
+      token: token,
+    },
+  });
+
+  socket.on("connect", () => {
+    console.log("Socket connected:", socket.id);
+  });
+
+  socket.on("connect_error",  (err: any) => {
+    console.log("Socket connection error:", err.message);
   });
 
   return socket;
