eb676e48bf79a1845ad9e43a3dfb1af8f62f9162
Abhinav2005-hub <abhinavsaini995@gmail.com>
2026-02-11 20:25:28 +0530

    use http server listen for socket.io

--- server/index.js
+++ server/index.js
@@ -38,6 +38,6 @@ const server = http.createServer(app);
 setupSocket(server);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
